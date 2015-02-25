5b1a26184069b869ad779e3e50059ffb5f0f3438
rosyswapna <rosy.swapna@gmail.com>
2015-02-25 15:13:47 +0530

    logo changed

--- config.php
+++ config.php
@@ -59,14 +59,14 @@ if (!isset($path_to_root) || isset($_GET['path_to_root']) || isset($_POST['path_
 		ini_set("log_errors", "On");
 	}		
 	// Main Title
-	$app_title = "FrontAccounting";
+	$app_title = "Smart Accounting";
 
 	// Build for development purposes
 	$build_version 	= date("d.m.Y", filemtime("$path_to_root/CHANGELOG.txt"));
 
 	// Powered by
-	$power_by 		= "FrontAccounting";
-	$power_url 		= "http://frontaccounting.com";
+	$power_by 		= "Smart Accounting";
+	$power_url 		= "http://acube.co";
 
 	/* No check on edit conflicts. Maybe needed to be set to 1 in certains Windows Servers */
 	$no_check_edit_conflicts = 0;
