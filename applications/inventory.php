4976f38baa7dddad71c24fe8da005303ca385a00
rosyswapna <rosy.swapna@gmail.com>
2015-03-12 14:53:58 +0530

    change menu items and hide freight charge

--- applications/inventory.php
+++ applications/inventory.php
@@ -18,33 +18,33 @@ class inventory_app extends application
 		$this->add_module(_("Transactions"));
 		$this->add_lapp_function(0, _("Inventory Location &Transfers"),
 			"inventory/transfers.php?NewTransfer=1", 'SA_LOCATIONTRANSFER', MENU_TRANSACTION);
-		$this->add_lapp_function(0, _("Inventory &Adjustments"),
+		$this->add_rapp_function(0, _("Inventory &Adjustments"),
 			"inventory/adjustments.php?NewAdjustment=1", 'SA_INVENTORYADJUSTMENT', MENU_TRANSACTION);
 
 		$this->add_module(_("Inquiries and Reports"));
-		$this->add_lapp_function(1, _("Inventory Item &Movements"),
+		$this->add_lapp_function(1, _("Inventory Item &Movement Reports"),
 			"inventory/inquiry/stock_movements.php?", 'SA_ITEMSTRANSVIEW', MENU_INQUIRY);
-		$this->add_lapp_function(1, _("Inventory Item &Status"),
+		$this->add_lapp_function(1, _("Inventory Item &Status Reports"),
 			"inventory/inquiry/stock_status.php?", 'SA_ITEMSSTATVIEW', MENU_INQUIRY);
-		$this->add_rapp_function(1, _("Inventory &Reports"),
+		$this->add_rapp_function(1, _("Inventory and Location &Reports"),
 			"reporting/reports_main.php?Class=2", 'SA_ITEMSTRANSVIEW', MENU_REPORT);
 
 		$this->add_module(_("Maintenance"));
-		$this->add_lapp_function(2, _("&Items"),
+		$this->add_lapp_function(2, _("Add and Manage &Items"),
 			"inventory/manage/items.php?", 'SA_ITEM', MENU_ENTRY);
-		$this->add_lapp_function(2, _("&Foreign Item Codes"),
+		/*$this->add_lapp_function(2, _("&Foreign Item Codes"),
 			"inventory/manage/item_codes.php?", 'SA_FORITEMCODE', MENU_MAINTENANCE);
 		$this->add_lapp_function(2, _("Sales &Kits"),
 			"inventory/manage/sales_kits.php?", 'SA_SALESKIT', MENU_MAINTENANCE);
 		$this->add_lapp_function(2, _("Item &Categories"),
-			"inventory/manage/item_categories.php?", 'SA_ITEMCATEGORY', MENU_MAINTENANCE);
-		$this->add_lapp_function(2, _("Inventory &Locations"),
+			"inventory/manage/item_categories.php?", 'SA_ITEMCATEGORY', MENU_MAINTENANCE);*/
+		$this->add_lapp_function(2, _("Add and Manage Inventory &Locations"),
 			"inventory/manage/locations.php?", 'SA_INVENTORYLOCATION', MENU_MAINTENANCE);
-		$this->add_rapp_function(2, _("Inventory &Movement Types"),
-			"inventory/manage/movement_types.php?", 'SA_INVENTORYMOVETYPE', MENU_MAINTENANCE);
-		$this->add_rapp_function(2, _("&Units of Measure"),
+		/*$this->add_rapp_function(2, _("Inventory &Movement Types"),
+			"inventory/manage/movement_types.php?", 'SA_INVENTORYMOVETYPE', MENU_MAINTENANCE);*/
+		$this->add_rapp_function(2, _("Add and Manage &Units of Measure"),
 			"inventory/manage/item_units.php?", 'SA_UOM', MENU_MAINTENANCE);
-		$this->add_rapp_function(2, _("&Reorder Levels"),
+		$this->add_rapp_function(2, _("Manage Inventory &Reorder Levels"),
 			"inventory/reorder_level.php?", 'SA_REORDER', MENU_MAINTENANCE);
 
 		$this->add_module(_("Pricing and Costs"));
